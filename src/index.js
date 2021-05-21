e4ddf300155e486daa2041c02b6519a782c91876
Nanlung <nanlung.temlong@gmail.com>
2021-05-21 18:29:21 +0100

    links added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,33 @@ const {ApolloServer} = require('apollo-server');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
   `
 
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographl.com',
+  description: "Fullstack tutorial for GraphQL"
+}]
+
 const resolvers = {
   Query: {
-    info: () => 'This is the Api of Hackernews Clone with GraphQL'
+    info: () => 'This is the Api of Hackernews Clone with GraphQL',
+
+    feed: () => links,
+  },
+
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 }
 
